eed6b895bc22d3c5bd6febc9d881bb9eb68c008c
tanisha0804 <58jaintanisha@gmail.com>
2026-01-20 12:09:41 +0530

    deleted unnecessary files

--- backend/app/core/skill_aggregator.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-from app.core.skill_ontology import normalize_skills
-
-def aggregate_resume_skills(skill_list: list) -> dict:
-    raw_counts = Counter(skill_list)
-    return normalize_skills(raw_counts)
